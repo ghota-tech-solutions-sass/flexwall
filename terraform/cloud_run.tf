aeab4e0b2d1aff7d0f5ebf3ed522fae651d5995a
villers mickael <villers@ghotatechsolutions.com>
2026-09-14 19:05:04 +0200

    feat(terraform): referral coupon and refund events for the webhook

--- terraform/cloud_run.tf
+++ terraform/cloud_run.tf
@@ -188,6 +188,7 @@ locals {
       STRIPE_PRICE_YEARLY          = stripe_price.yearly.id
       STRIPE_PRICE_LIFETIME        = stripe_price.lifetime.id
       STRIPE_PORTAL_CONFIGURATION  = stripe_portal_configuration.billing.id
+      STRIPE_REFERRAL_COUPON       = stripe_coupon.referral.id
       STRIPE_AUTOMATIC_TAX         = tostring(var.stripe_automatic_tax)
       STRIPE_COLLECT_TERMS_CONSENT = tostring(var.stripe_collect_terms_consent)
     },
